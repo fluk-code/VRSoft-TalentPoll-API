545119967aab84cdc960e9fe5c5c3af70e7be18e
Felipe Ferreira <ferreira.felipe@vrsoft.com.br>
2024-07-09 13:08:56 +0000

    TASK-001: feat: add ValueObject Descricao from shared project

---
+++ src/@shared/domain/value-objects/descricao.spec.ts
@@ -0,0 +1,31 @@
+import { Descricao } from './descricao.vo';
+
+describe(`${Descricao.name}`, () => {
+  describe(`${Descricao.factory.name}`, () => {
+    it('Deve retornar um "Either Right" com a Descricao quando o valor inserido for valido', () => {
+      const validValue = 'Qualquer valor valido';
+      const either = Descricao.factory(validValue);
+      expect(either.isRight()).toBeTruthy();
+
+      const vo = either.value as Descricao;
+      expect(vo.value).toBe(validValue);
+    });
+
+    it('Deve retornar "Either Left" com o motivo da rejeição quando valor inserido for diferente de string', () => {
+      const arrangeInvalidValues = [true, false, 0, 1];
+
+      for (const invalidValues of arrangeInvalidValues) {
+        const either = Descricao.factory(invalidValues as unknown as string);
+        expect(either.isLeft()).toBeTruthy();
+        expect(either.value).toBe('descricao deve ser uma string com no máximo 60 caracteres');
+      }
+    });
+
+    it('Deve retornar "Either Left" com o motivo da rejeição quando valor inserido for string maior que 60 caracteres', () => {
+      const invalidValue = '1234567890123456789012345678901234567890123456789012345678901';
+      const either = Descricao.factory(invalidValue as unknown as string);
+      expect(either.isLeft()).toBeTruthy();
+      expect(either.value).toBe('descricao deve ser uma string com no máximo 60 caracteres');
+    });
+  });
+});
