450cd914651f10daaa26b7b2738b2af4952470fd
Felipe Ferreira <ferreira.felipe@vrsoft.com.br>
2024-07-09 20:54:59 +0000

    TASK-001: feat: add use case UpdateLoja

--- src/loja/domain/repositories/loja.repository.interface.ts
+++ src/loja/domain/repositories/loja.repository.interface.ts
@@ -1,5 +1,14 @@
 import { LojaDTO } from '../dtos/loja.dto';
+import { Loja } from '../entities/loja.entity';
 
 export interface ISavableLoja {
   save(descricao: Omit<LojaDTO, 'id'>): Promise<LojaDTO>;
+}
+
+export interface IFindableLojaById {
+  findById(id: number): Promise<LojaDTO | null>;
+}
+
+export interface IUpdatableLoja {
+  update(loja: Loja): Promise<void>;
 }
