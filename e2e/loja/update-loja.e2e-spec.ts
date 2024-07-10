c7b816c85a6abf820962cfabfe0db548d65e05e4
Felipe Ferreira <ferreira.felipe@vrsoft.com.br>
2024-07-10 00:31:22 +0000

    TASK-001: test: cov update loja flow

--- e2e/loja/update-loja.e2e-spec.ts
+++ e2e/loja/update-loja.e2e-spec.ts
@@ -1,4 +1,5 @@
-import { request } from 'superagent';
+/* eslint-disable sonarjs/no-duplicate-string */
+import request from 'supertest';
 import { DataSource } from 'typeorm';
 
 import { HttpStatus, INestApplication } from '@nestjs/common';
@@ -7,6 +8,8 @@ import { TestingModule } from '@nestjs/testing';
 import LojaTypeOrm from '../../src/loja/infra/data/typeorm/entities/loja-typeorm.entity';
 import { LojaTypeOrmRepository } from '../../src/loja/infra/data/typeorm/repositories/loja-typeorm.repository';
 import appSetup from '../utils/app-setup';
+
+let id: number;
 
 describe('[POST] /api/v1/lojas', () => {
   let app: INestApplication;
@@ -18,11 +21,19 @@ describe('[POST] /api/v1/lojas', () => {
     app = nestApplication;
     module = moduleFixture;
     dataSource = moduleFixture.get<DataSource>(DataSource);
+  });
 
-    await dataSource.query(`
-      INSERT INTO "usuario_keeps"("descricao")
-      VALUES ('Some description')
-    `);
+  beforeEach(async () => {
+    const output = await dataSource
+      .createQueryBuilder()
+      .insert()
+      .into(LojaTypeOrm)
+      .values({
+        descricao: 'Some descriptions',
+      })
+      .execute();
+
+    id = output.raw[0].id;
   });
 
   afterEach(async () => {
@@ -31,28 +42,79 @@ describe('[POST] /api/v1/lojas', () => {
 
   it('deve retornar 200 quando a chamada for bem sucedida', async () => {
     const response = await request(app.getHttpServer())
-      .post(`/lojas`)
+      .put(`/lojas/${id}`)
       .set('Content-Type', 'application/json')
       .send({
         descricao: 'New description',
       });
 
     expect(response.statusCode).toEqual(HttpStatus.OK);
-    expect(response.body).toEqual({
-      id: 1,
+    expect(response.body).toStrictEqual({
+      id,
       descricao: 'New description',
     });
   });
 
-  it('deve retornar 500 quando ocorrer um erro inesperado', async () => {
+  it('deve retornar 400 quando o body for invalido', async () => {
+    const response = await request(app.getHttpServer())
+      .put(`/lojas/${id}`)
+      .set('Content-Type', 'application/json')
+      .send();
+
+    expect(response.statusCode).toEqual(HttpStatus.BAD_REQUEST);
+  });
+
+  it('deve retornar 404 quando o id nao for encontrado', async () => {
+    const response = await request(app.getHttpServer())
+      .put(`/lojas/0`)
+      .set('Content-Type', 'application/json')
+      .send({
+        descricao: 'New description',
+      });
+
+    expect(response.statusCode).toEqual(HttpStatus.NOT_FOUND);
+  });
+
+  it('deve retornar 422 quando o id nao for encontrado', async () => {
+    const repository = module.get<LojaTypeOrmRepository>(LojaTypeOrmRepository);
+    jest.spyOn(repository, 'findById').mockResolvedValueOnce({
+      id: 0,
+      descricao: 'Some description',
+    });
+
+    const response = await request(app.getHttpServer())
+      .put(`/lojas/0`)
+      .set('Content-Type', 'application/json')
+      .send({
+        descricao: 'New description',
+      });
+
+    expect(response.statusCode).toEqual(HttpStatus.UNPROCESSABLE_ENTITY);
+  });
+
+  it('deve retornar 500 quando ocorrer um erro inesperado na busca do recurso', async () => {
     const repository = module.get<LojaTypeOrmRepository>(LojaTypeOrmRepository);
     jest.spyOn(repository, 'findById').mockRejectedValueOnce(new Error('Some message'));
 
     const response = await request(app.getHttpServer())
-      .post(`/lojas`)
+      .put(`/lojas/${id}`)
       .set('Content-Type', 'application/json')
       .send({
-        descricao: 'Some description',
+        descricao: 'New description',
+      });
+
+    expect(response.statusCode).toEqual(HttpStatus.INTERNAL_SERVER_ERROR);
+  });
+
+  it('deve retornar 500 quando ocorrer um erro inesperado na atualizacao do recurso', async () => {
+    const repository = module.get<LojaTypeOrmRepository>(LojaTypeOrmRepository);
+    jest.spyOn(repository, 'update').mockRejectedValueOnce(new Error('Some message'));
+
+    const response = await request(app.getHttpServer())
+      .put(`/lojas/${id}`)
+      .set('Content-Type', 'application/json')
+      .send({
+        descricao: 'New description',
       });
 
     expect(response.statusCode).toEqual(HttpStatus.INTERNAL_SERVER_ERROR);
--- src/loja/loja.provider.ts
+++ src/loja/loja.provider.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-namespace */
 
 import { CreateLojaUseCase } from './applications/use-cases/create-loja.use-case';
-import { ISavableLoja } from './domain/repositories/loja.repository.interface';
+import { UpdateLojaUseCase } from './applications/use-cases/update-loja.use-case';
+import {
+  IFindableLojaById,
+  ISavableLoja,
+  IUpdatableLoja,
+} from './domain/repositories/loja.repository.interface';
 import { LojaTypeOrmRepository } from './infra/data/typeorm/repositories/loja-typeorm.repository';
 
 export namespace LOJA {
@@ -9,6 +14,13 @@ export namespace LOJA {
     export const CRATE_LOJA_USE_CASE = {
       provide: CreateLojaUseCase,
       useFactory: (repository: ISavableLoja) => new CreateLojaUseCase(repository),
+      inject: [LojaTypeOrmRepository],
+    };
+
+    export const UPDATE_LOJA_USE_CASE = {
+      provide: UpdateLojaUseCase,
+      useFactory: (repository: IFindableLojaById & IUpdatableLoja) =>
+        new UpdateLojaUseCase(repository),
       inject: [LojaTypeOrmRepository],
     };
   }
